522217ecfbe7223637a0389628b51c0faf2405ca
thomas <thomas.vos1@gmail.com>
2020-02-12 16:21:08 +0100

    Fix sudokusolver.hs

--- week2-team/SudokuSolver.hs
+++ week2-team/SudokuSolver.hs
@@ -1,17 +1,12 @@
 module SudokuSolver
 where
-import
-import
-import
-import
-Sudoku
-Data.List
-Data.List.Split
-Data.Maybe
+import Sudoku
+import Data.List
+import Data.List.Split
+import Data.Maybe
 positions, values :: [Int]
 positions = [1..9]
-values
-= [1..9]
+values = [1..9]
 blocks :: [[Int]]
 blocks = [[1..3], [4..6], [7..9]]
 showDgt :: Value -> String
@@ -34,17 +29,13 @@ intercalate "\n+-------+-------+-------+\n" rows
 ++
 "\n+-------+-------+-------+"
 where rows = map (intercalate "\n") $ chunksOf 3 $ map showRow grid
-                                                    sud2grid :: Sudoku -> Grid
-
+sud2grid :: Sudoku -> Grid
+3
 sud2grid s =
 [ [ s (r,c) | c <- [1..9] ] | r <- [1..9] ]
-grid2sud ::
-grid2sud gr
-where pos
-pos
-Grid -> Sudoku
-= \(r,c) -> pos gr (r,c)
-:: [[a]] -> (Row,Column) -> a
-gr (r,c) = (gr !! (r - 1)) !! (c - 1)
+grid2sud :: Grid -> Sudoku
+grid2sud gr = \(r,c) -> pos gr (r,c)
+where pos :: [[a]] -> (Row,Column) -> a
+pos gr (r,c) = (gr !! (r - 1)) !! (c - 1)
 printSudoku :: Sudoku -> IO()
 printSudoku = putStrLn . showGrid . sud2grid
