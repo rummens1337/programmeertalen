fae22a5f698f4de94724e33872aaa41074a8b5f3
thomas <thomas.vos1@gmail.com>
2020-02-13 21:10:42 +0100

    FreeInRow/Column row/column/subgridvalid zijn af, consistent werkt vermoedelijk ook, extend/freeinsubgrid/freeatpos/openpositions nog niet af.

--- week2-team/SudokuSolver.hs
+++ week2-team/SudokuSolver.hs
@@ -1,15 +1,34 @@
+-- Namen: Thomas Vos, Michel Rummens
+-- Studentnummers: 12829501, 13108093
+-- This program can solve a sudoku using several functions and a binary tree.
+
 module SudokuSolver where
 
 import Sudoku
 import Data.List
-import Data.List.Split
 import Data.Maybe
+
+testGrid :: Grid -- Testgrid, weghalen voor inleveren
+testGrid =
+  [ [1,1,1,0,0,0,7,0,9]
+  , [0,0,0,7,8,9,1,2,3] -- 4,5,6
+  , [7,8,9,1,2,3,4,5,6]
+  , [2,3,1,6,7,4,8,9,5]
+  , [8,7,5,9,1,2,0,6,4]
+  , [6,9,4,5,3,8,2,1,7]
+  , [3,1,7,2,6,5,9,4,8]
+  , [5,4,2,8,9,7,6,3,1]
+  , [9,6,8,3,4,1,5,7,2]
+  ]
+
+testSudoku :: Sudoku -- Testsudoku, weghalen voor inleveren
+testSudoku = grid2sud testGrid
 
 positions, values :: [Int]
 positions = [1..9]
 values = [1..9]
 
-blocks :: [[Int]]
+blocks :: [[Int]] -- ?
 blocks = [[1..3], [4..6], [7..9]]
 
 showDgt :: Value -> String
@@ -18,6 +37,14 @@ showDgt d = show d
 
 showSubgridRow:: [Value] -> String
 showSubgridRow = unwords . map showDgt
+
+{-- The chunksOf n xs ++ chunksOf n ys == chunksOf n (xs ++ ys)
+property holds.
+--}
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf _ [] = [] -- Models the Data.List.Split implementation
+chunksOf n ls = fst splitted : (chunksOf n . snd) splitted where splitted = splitAt n ls
 
 showRow :: [Value] -> String
 showRow sr = "| " ++ intercalate " | " (map showSubgridRow $ chunksOf 3 sr) ++ " |"
@@ -36,6 +63,44 @@ grid2sud gr = \(r,c) -> pos gr (r,c)
 printSudoku :: Sudoku -> IO()
 printSudoku = putStrLn . showGrid . sud2grid
 
+-- [1..9] \\ [row ofzo] (list difference function, yoinked waardes die overeenkomen uit left array)
+
+-- step 1
+
+-- extend :: Sudoku -> (Row,Column,Value) -> Sudoku
+-- extend s (r,c,v) = doe iets lol
+
+freeInRow :: Sudoku -> Row -> [Value] -- Werkt.
+freeInRow s r = [1..9] \\ ((sud2grid s) !! (r - 1))
+
+freeInColumn :: Sudoku -> Column -> [Value] -- Werkt.
+freeInColumn s c = [1..9] \\ (foldr(\x acc -> x !! (c - 1) : acc) [] (sud2grid s))
+
+freeInSubgrid :: Sudoku -> (Row,Column) -> [Value] -- ?
+freeInSubgrid s (r,c) = [1..9] \\ (quot r 3, quot c 3)
+
+-- freeAtPos :: Sudoku -> (Row,Column) -> [Value]
+-- freeAtPos s (r,c) = doe iets lol
+
+-- openPositions :: Sudoku -> [(Row,Column)]
+-- openPositions s = doe iets lol
+
+-- step 2
+
+rowValid :: Sudoku -> Row -> Bool -- Werkt.
+rowValid s r = (freeInRow s r) == []
+
+colValid :: Sudoku -> Column -> Bool -- Werkt.
+colValid s c = (freeInColumn s c) == []
+
+subgridValid :: Sudoku -> (Row,Column) -> Bool -- Werkt wss.
+subgridValid s (r,c) = (freeInSubgrid s (r,c)) == []
+
+consistent :: Sudoku -> Bool
+consistent s = foldr(\x acc -> (rowValid x (sud2grid s)) && (columnValid x (sud2grid s)) && -- Checkt of alle rows en columns valid zijn.
+               (foldr(\y acc -> (subgridValid (x,y) s) && acc) True [1..9]) && -- Checkt of alle subgrids valid zijn.
+               acc) True [1..9]
+
 
 -- Step 1 - Create list with empty positions in the given sudoku
 --extend :: Sudoku -> (Row,Column,Value) -> Sudoku
@@ -46,7 +111,7 @@ printSudoku = putStrLn . showGrid . sud2grid
 --openPositions :: Sudoku -> [(Row,Column)]
 
 -- Step 2 - Check if sudoku is valid
---rowValid :: Sudoku -> Row -> Bool
+--rowValid :: Sudoku -> Row -> Bool Hergebruik freeInRowfuncties!
 --colValid :: Sudoku -> Column -> Bool
 --subgridValid :: Sudoku -> (Row,Column) -> Bool
 --consistent :: Sudoku -> Bool
