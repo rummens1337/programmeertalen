a69c960c68490d8a39e65f0bf5295a745ade8f9d
thomas <thomas.vos1@gmail.com>
2020-02-14 21:46:08 +0100

    All functions work W00T W00T

--- week2-team/SudokuSolver.hs
+++ week2-team/SudokuSolver.hs
@@ -10,11 +10,11 @@ import Data.Maybe
 
 testGrid :: Grid -- Testgrid, weghalen voor inleveren
 testGrid =
-  [ [1,1,1,0,0,0,7,0,9]
-  , [0,0,0,7,8,9,1,2,3] -- 4,5,6
+  [ [1,0,3,4,5,6,7,8,9]
+  , [4,5,6,7,8,9,1,2,3]
   , [7,8,9,1,2,3,4,5,6]
   , [2,3,1,6,7,4,8,9,5]
-  , [8,7,5,9,1,2,0,6,4]
+  , [8,7,5,9,1,2,3,6,4]
   , [6,9,4,5,3,8,2,1,7]
   , [3,1,7,2,6,5,9,4,8]
   , [5,4,2,8,9,7,6,3,1]
@@ -63,75 +63,94 @@ grid2sud gr = \(r,c) -> pos gr (r,c)
 printSudoku :: Sudoku -> IO()
 printSudoku = putStrLn . showGrid . sud2grid
 
--- [1..9] \\ [row ofzo] (list difference function, yoinked waardes die overeenkomen uit left array)
+-- FUNCTIONS THAT WERE REQUIRED TO BE ADDED
 
--- step 1
+-- Returns the sudoku with one value changed.
 
-extend :: Sudoku -> (Row,Column,Value) -> Sudoku -- WERKT NOG NIET
-extend s (r,c,v) = doe iets lol
+extend :: Sudoku -> (Row,Column,Value) -> Sudoku
+extend s (r,c,v) = grid2sud (foldr(\z acc -> if z == r then ((init xs) ++ [v] ++ ys) : acc else (grid !! (z-1)) : acc) [] [1..9])
+   where (xs,ys) = splitAt (c) ((sud2grid s) !! (r - 1))
+         grid = sud2grid s
 
-freeInRow :: Sudoku -> Row -> [Value] -- Werkt.
+-- Returns a list of every number available for one specific row.
+
+freeInRow :: Sudoku -> Row -> [Value]
 freeInRow s r = [1..9] \\ ((sud2grid s) !! (r - 1))
 
-freeInColumn :: Sudoku -> Column -> [Value] -- Werkt.
+-- Returns a list of every number available for one specific column.
+
+freeInColumn :: Sudoku -> Column -> [Value]
 freeInColumn s c = [1..9] \\ (foldr(\x acc -> x !! (c - 1) : acc) [] (sud2grid s))
 
--- freeInSubgrid :: Sudoku -> (Row,Column) -> [Value] -- WERKT NOG NIET, wss tuple met fst en snd uit
--- freeInSubgrid s (r,c) = [1..9] \\ (quot r 3, quot c 3)
-
--- Shit van internet
-
-valAt' :: Sudoku -> (Row, Value) -> Value
-valAt' (i,j) = do
-               a <- get
-               return (a ! (i,j))
+-- Returns a list of every number available for one specific subgrid.
 
 freeInSubgrid :: Sudoku -> (Row,Column) -> [Value]
-mapM valAt' [(i' + u, j' + v) | u <- [1..3], v <- [1..3]]
-  where i' = ((i-1) `div` 3) * 3
-        j' = ((j-1) `div` 3) * 3
+freeInSubgrid s (r,c) = [1..9] \\ (getValues s (topLeftPoint(r,c)))
 
--- ^^
+-- Returns a list of every number available for one specific position.
 
-freeAtPos :: Sudoku -> (Row,Column) -> [Value] -- Werkt wss.
-freeAtPos s (r,c) = [1..9] \\ ((freeInRow s r) ++ (freeInColumn s c) ++ (freeInSubgrid s (r,c)))
+freeAtPos :: Sudoku -> (Row,Column) -> [Value]
+freeAtPos s (r,c) = (((freeInRow s r) ++ (freeInColumn s c) ++ (freeInSubgrid s (r,c))) \\ [1..9]) \\ [1..9]
 
-openPositions :: Sudoku -> [(Row,Column)] -- Werkt.
+-- Returns a list of every open position.
+
+openPositions :: Sudoku -> [(Row,Column)]
 openPositions s = concat (foldr(\x acc -> zip [x,x..] (openPosColumn s x) : acc) [] [1..9])
 
--- step 2
 
-rowValid :: Sudoku -> Row -> Bool -- Werkt.
+
+-- Returns if a row is valid (no empty spots and no double numbers).
+
+rowValid :: Sudoku -> Row -> Bool
 rowValid s r = (freeInRow s r) == []
 
-colValid :: Sudoku -> Column -> Bool -- Werkt.
+-- Returns if a column is valid (no empty spots and no double numbers).
+
+colValid :: Sudoku -> Column -> Bool
 colValid s c = (freeInColumn s c) == []
 
-subgridValid :: Sudoku -> (Row,Column) -> Bool -- Werkt wss.
+-- Returns if a subgrid is valid (no empty spots and no double numbers).
+
+subgridValid :: Sudoku -> (Row,Column) -> Bool
 subgridValid s (r,c) = (freeInSubgrid s (r,c)) == []
 
-consistent :: Sudoku -> Bool -- Werkt theoretisch gezien wel, nog niet kunnen testen (WERKT DUS WSS NOG NIET)
-consistent s = foldr(\x acc -> (rowValid x (sud2grid s)) && (columnValid x (sud2grid s)) && -- Checkt of alle rows en columns valid zijn.
-               (foldr(\y acc -> (subgridValid (x,y) s) && acc) True [1..9]) && -- Checkt of alle subgrids valid zijn.
+-- Returns if a sudoku is valid (no empty spots and no double numbers).
+
+consistent :: Sudoku -> Bool
+consistent s = foldr(\x acc -> (rowValid s x ) && (colValid s x) &&
+               (foldr(\y acc -> (subgridValid s (x,y)) && acc) True [1..9]) &&
                acc) True [1..9]
 
--- Extra functies
+-- EXTRA FUNCTIONS
 
 -- Returns all the indices of the empty spots of a row, and therefore the column numbers.
 
 openPosColumn :: Sudoku -> Row -> [(Value)]
 openPosColumn s r = foldr (\x acc -> x + 1 : acc) [] (elemIndices 0 ((sud2grid s) !! (r - 1)))
 
--- Step 1 - Create list with empty positions in the given sudoku
---extend :: Sudoku -> (Row,Column,Value) -> Sudoku
---freeInRow :: Sudoku -> Row -> [Value]
---freeInColumn :: Sudoku -> Column -> [Value]
---freeInSubgrid :: Sudoku -> (Row,Column) -> [Value]
---freeAtPos :: Sudoku -> (Row,Column) -> [Value]
---openPositions :: Sudoku -> [(Row,Column)]
+-- Returns the top left point (x,y) in a subgrid.
 
--- Step 2 - Check if sudoku is valid
---rowValid :: Sudoku -> Row -> Bool Hergebruik freeInRowfuncties!
---colValid :: Sudoku -> Column -> Bool
---subgridValid :: Sudoku -> (Row,Column) -> Bool
---consistent :: Sudoku -> Bool
+topLeftPoint :: (Row, Column) -> (Row, Column)
+topLeftPoint (r,c) = (x,y)
+             where x = ((quot (r - 1) 3) * 3) + 1
+                   y = ((quot (c - 1) 3) * 3) + 1
+
+-- Returns all values of a subgrid.
+
+getValues :: Sudoku -> (Row, Column) -> [Value]
+getValues s (r,c) = foldr(\x acc -> (getValueAtPos s x) : acc) [] allPos
+     where allPos = allPosSubgrid (r,c)
+
+-- Returns the value of a certain position.
+
+getValueAtPos :: Sudoku -> (Row, Column) -> Value
+getValueAtPos s (r,c) = ((sud2grid s) !! (r - 1)) !! (c - 1)
+
+-- Returns all positions of a subgrid.
+
+allPosSubgrid :: (Row,Column) -> [(Row,Column)]
+allPosSubgrid (r,c) = [ (rOther,cOther) | rOther<-y, cOther<-x ]
+      where y = [r..r + 2]
+            x = [c..c + 2]
+
+
