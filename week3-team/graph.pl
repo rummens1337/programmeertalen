ed0af926064d4bfea0039acffe1837966b4df3b6
thomas <thomas.vos1@gmail.com>
2020-02-22 22:11:14 +0100

    Now first assignment takes path(F,T,Path) without length

--- week3-team/graph.pl
+++ week3-team/graph.pl
@@ -21,10 +21,10 @@ edge(5,4,2).
 %  van https://www.cpp.edu/~jrfisher/www/prolog_tutorial/2_15A.pl
 connected(X,Y,L) :- edge(X,Y,L).
 
-%    F T Vis  Path
-path(A,B,Path,Len) :-
-       travel(A,B,[],Q,Len),
-       reverse(Q,Path).
+%    F T Path
+path(F,T,Path) :-
+       travel(F,T,[],Visited,_),
+       reverse(Visited,Path).
 
 travel(A,B,P,[edge(A,B,L) |P ],L) :-
        connected(A,B,L).
@@ -41,6 +41,7 @@ travel(A,B,Visited,Path,L) :-
 % path(X, Y, _, Path) :-
 %     edge(X, Y, C),
 %     Path = [edge(X, Y, C)].
+
 
 
 
