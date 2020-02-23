bf200262f97b40fc41655cbbfe5ce8a7c1685e79
Michel <michelrummens@hotmail.com>
2020-02-23 15:27:11 +0900

    query3

--- week3-team/queries.pl
+++ week3-team/queries.pl
@@ -1,21 +1,34 @@
 :- ensure_loaded(solution1).
 :- ensure_loaded(solution2).
-:- consult(solution2).
 
 query1() :-
-    forall(path(1, 5, Path), writeln("1..5 " = Path)),
+    writeln("________ query1 ________"),
+    forall(path(1, 5, Path), writeln("1..5 "=Path)),
     nl,
-    forall(path(3, 5, Path), writeln("3..5 " = Path)),
+    forall(path(3, 5, Path), writeln("3..5 "=Path)),
     nl,
-    forall(path(5, 4, Path), writeln("5..4 " = Path)),
+    forall(path(5, 4, Path), writeln("5..4 "=Path)),
     nl.
 
 query2() :-
+    writeln("________ query2 ________"),
     path(1, 5, Path),
-    writeln("Path " = Path),
+    writeln("Path "=Path),
     cost(Path, Cost),
-    writeln("Cost " = Cost).
+    writeln("Cost "=Cost),
+    nl.
+
+query3() :-
+    writeln("________ query3 ________"),
+    forall(shortestPath(1, 3, Path),
+           writeln("Shortest path 1..3 "=Path)),
+    forall(shortestPath(3, 5, Path),
+           writeln("Shortest path 3..5 "=Path)),
+    forall(shortestPath(5, 4, Path),
+           writeln("Shortest path 5..4 "=Path)),
+    nl.
 
 % Can be commented if they are to be called manually.
 :- query1.
 :- query2.
+:- query3. 
