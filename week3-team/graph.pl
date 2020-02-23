f51934da4162a0acbc5285e62ae5b49ef8b1980e
thomas <thomas.vos1@gmail.com>
2020-02-23 15:36:19 +0100

    Comments baybee

--- week3-team/graph.pl
+++ week3-team/graph.pl
@@ -1,6 +1,6 @@
 % Namen: Thomas Vos, Michel Rummens
 % Studentnummers: 12829501, 13108093
-% Lovely assignment!
+% Dit bestand definieerd de edges van de graph van opdracht 1 en 2.
 
 % F = From
 % T = To
@@ -16,61 +16,3 @@ edge(3,1,9).
 edge(3,2,2).
 edge(5,1,3).
 edge(5,4,2).
-
-%  tot hier
-
-% Deze functie niet veranderen, deze geeft een soort van goed resultaat!
-% path(X, Y, _, Path) :-
-%     edge(X, Y, C),
-%     Path = [edge(X, Y, C)].
-
-
-
-
-
-
-
-
-
-% path(From, To, Path) :-
-%     path(From, To, Visited, Path).
-
-% path(From, To, Visited, Path) :-
-%     R = [],
-%     Path = [],
-%     edge(To,Y,Z),
-%     append([Y], R, Visited),
-%     write(Visited).
-
-
-% connected(X,Y,L) :- E = edge(X,Y,L).
-% connected(X,Y,L) :- edge(Y,X,L).
-
-% is_connected(E1, E2, L) :- edge(E1, E2, _).
-% is_connected(E1, E2, L) :- edge(E1, R, _) , is_connected(R, E2, L).
-
-
-% addition(X,Y,Z) :- Z is X+Y.
-% add(X,Y):-
-%    between(X,Y,A),
-%    addition(X,A,Z),
-%    writeln(addition(X,A,Z)),
-%    X1 is X+1,
-%    add(X1,Y).
-
-%! FACTS
-% bigger(elephant, horse).
-% bigger(horse, donkey).
-% bigger(donkey, dog).
-% bigger(donkey, monkey).
-
-% bigger(elephant,X). yields -> horse
-% is_bigger(elephant,X). yields -> dog, monkey, donkey, horse
-%
-% (elephant,dog) ==> False
-% (elephant, Z=horse) -> (Z=horse,dog) ==> False
-% (horse, Z=donkey) -> (Z=donkey,dog) ==> True
-%
-% RULES
-% is_bigger(X, Y) :- bigger(X, Y).
-% is_bigger(X, Y) :- bigger(X, Z), is_bigger(Z, Y).
--- week3-team/solution2.pl
+++ week3-team/solution2.pl
@@ -1,11 +1,24 @@
+% Namen: Thomas Vos, Michel Rummens
+% Studentnummers: 12829501, 13108093
+% In dit bestand staan predikaten die de kosten van een pad kunnen berekenen,
+% en een predikaat dat het kortste pad tussen twee knopen kan bepalen.
+
 :- consult('graph.pl').
 :- consult('solution1.pl').
 
-cost([], 0).
+% "cost" berekent de kosten van een pad, door steeds de kost van een edge op te
+% tellen bij het totaal met behulp van recursie.
 
+cost([], 0).
 cost([edge(_, _, C)|RestOfPath], Cost) :-
     cost(RestOfPath, NewCost),
     Cost is C+NewCost.
+
+%"shortestPath" (niet helemaal idiomatisch overigens maar zo moest het wel heten
+% in de opdracht) berekent het kortste pad uit een lijst met paden, door met een
+% findall alle paden + bijbehorende kosten in de vorm van tuples in een lijst te
+% zetten, die vervolgens te sorteren van weinig kosten naar veel kosten en
+% van die gesorteerde lijst de eerste (head) te pakken.
 
 shortestPath(F, T, Path) :-
     findall((Newpath, C),
