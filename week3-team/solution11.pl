285ce50cd754c7a7259f07345b30a32617b1ad1c
Michel <michelrummens@hotmail.com>
2020-02-23 12:35:51 +0900

    test

--- week3-team/solution11.pl
+++ week3-team/solution11.pl
@@ -1,12 +1,12 @@
-:- consult(graph).
+% :- consult(graph).
 
-path(From, To, Path) :-
-    path(From, To, [], Path).
+% path(From, To, Path) :-
+%     path(From, To, [], Path).
 
-path(To, To, Visited,Path):-
-    reverse(Visited, Path),!.
+% path(To, To, Visited,Path):-
+%     reverse(Visited, Path),!.
 
-path(From, To, Visited, Path):-
-    edge(From, X, Cost),
-    not(member(edge(_, X, _), Visited)),
-    path(X, To, [edge(From, X, Cost)|Visited], Path).
+% path(From, To, Visited, Path):-
+%     edge(From, X, Cost),
+%     not(member(edge(_, X, _), Visited)),
+%     path(X, To, [edge(From, X, Cost)|Visited], Path).
--- week3-team/solution2.pl
+++ week3-team/solution2.pl
@@ -1,20 +1,20 @@
-:- consult('graph.pl').
-:- consult('solution1.pl').
+% :- consult('graph.pl').
+% :- consult('solution1.pl').
 
-cost([],0).
-cost([edge(_,_,C) | RestOfPath], Cost) :-
-       cost(RestOfPath, NewCost),
-       Cost is C+NewCost.
+% cost([],0).
+% cost([edge(_,_,C) | RestOfPath], Cost) :-
+%        cost(RestOfPath, NewCost),
+%        Cost is C+NewCost.
 
-shortestPath(F,T,Path) :-
-       findall(path(F,T,Path), path(F,T,Path), List), % Berekent alle paden in 1 keer en zet ze in een list.
-       addCost(List,[]),
-       sort(List, Path).
-       %SortedList = [ Head | Tail],
-       %Path is Head.
+% shortestPath(F,T,Path) :-
+%        findall(path(F,T,Path), path(F,T,Path), List), % Berekent alle paden in 1 keer en zet ze in een list.
+%        addCost(List,[]),
+%        sort(List, Path).
+%        %SortedList = [ Head | Tail],
+%        %Path is Head.
 
-addCost([],_).
-addCost([H | T], List) :-
-       cost(H,Cost),
-       append([H], [Cost], Newlist),
-       addCost(T, Newlist).
+% addCost([],_).
+% addCost([H | T], List) :-
+%        cost(H,Cost),
+%        append([H], [Cost], Newlist),
+%        addCost(T, Newlist).
