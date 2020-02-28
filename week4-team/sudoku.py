4d9522c2bc09b2e3bdb46e2e0e96316aac7fdd1a
Michel <michelrummens@hotmail.com>
2020-02-28 14:25:53 +0900

    working version

--- week4-team/sudoku.py
+++ week4-team/sudoku.py
@@ -3,8 +3,8 @@ from collections import deque
 from copy import deepcopy
 
 # Declare global variables.
-box_y_len = 0
-box_x_len = 0
+grid_y_len = 0
+grid_x_len = 0
 sudoku_max_size = 0
 sudoku_min_size = 0
 sudoku_set = []
@@ -19,7 +19,8 @@ def create_sudoku(filename):
 
 # Initialize global variables, needed in multiple functions.
 def initialize_board(board):
-    global box_x_len, box_y_len, sudoku_max_size, sudoku_min_size, sudoku_set
+    global grid_x_len, grid_y_len, sudoku_max_size, sudoku_min_size, sudoku_set
+    
     sudoku = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     hexadoku = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
@@ -27,7 +28,8 @@ def initialize_board(board):
 
     [exit("wrong sudoku size") for row in board if sudoku_max_size != len(row)]
 
-    # sudoku_size : [block_x, block_y, sudoku_minimum]
+    # Sudoku sizes and their respective values.
+    # {sudoku_size} : [{block_x}, {block_y}, {sudoku_set}, {sudoku_minimum}]
     blockSize = {
         4: [2, 2, sudoku, 1],
         6: [3, 2, sudoku, 1],
@@ -38,9 +40,9 @@ def initialize_board(board):
         36: [6, 6, hexadoku, 0],
     }
 
-    box_x_len, box_y_len, sudoku_set, sudoku_min_size = blockSize[sudoku_max_size]
+    grid_x_len, grid_y_len, sudoku_set, sudoku_min_size = blockSize[sudoku_max_size]
 
-
+# Prints the board
 def print_board(board):
     for i in board:
         for n in i:
@@ -49,15 +51,15 @@ def print_board(board):
 
 
 # Finds empty positions in sudoku.
-def find_empty(bo):
+def find_empty(board):
     for i in range(sudoku_max_size):
         for j in range(sudoku_max_size):
-            if bo[i][j] == 0:
+            if board[i][j] == 0:
                 return (i, j)  # row, col
 
     return None
 
-
+# Checks if num on pos is a valid position
 def valid(board, num, pos):
     # Check row
     for i in range(sudoku_max_size):
@@ -69,31 +71,27 @@ def valid(board, num, pos):
         if board[i][pos[1]] == num and pos[0] != i:
             return False
 
-    # Check box
-    box_x = pos[1] // box_x_len
-    box_y = pos[0] // box_y_len
+    # Check grid
+    grid_x = pos[1] // grid_x_len
+    grid_y = pos[0] // grid_y_len
 
-    for i in range(box_y * box_y_len, box_y * box_y_len + box_y_len):
-        for j in range(box_x * box_x_len, box_x * box_x_len + box_x_len):
+    for i in range(grid_y * grid_y_len, grid_y * grid_y_len + grid_y_len):
+        for j in range(grid_x * grid_x_len, grid_x * grid_x_len + grid_x_len):
             if board[i][j] == num and (i, j) != pos:
                 return False
 
     return True
 
-
+# Solve the sudoku
 def solve(board):
     solved = deque()
     solved.append(board)
 
     while solved:
-        # Pick element from stack
         board = solved.pop()
-        # board = deepcopy(bo)
-
         find = find_empty(board)
 
         if not find:
-            print("----------------")
             print_board(board)
             return True
         else:
@@ -101,25 +99,28 @@ def solve(board):
 
         for i in range(sudoku_min_size, sudoku_max_size + 1):
             if valid(board, i, (row, col)):
-                # print([row,col], i)
                 board[row][col] = i
                 solved.append(deepcopy(board))
     return True
-    
+
+# 8 hour attempt to use a lot less memory.
+# R.I.P attempt, for you were a beautifull one.
 
 # def solve(board):
 #     solved = deque()
 #     row, col = find_empty(board)
 #     solved.append([board, sudoku_min_size, (row, col)])
 #     success = False
+#     backtrack = False
 
 #     while 1:
 #         # Pick element from stack
-#         bo = solved.pop()
-#         tmp_board = deepcopy(bo[0])  # Finding this error has taken us ages..
+#         board = solved.pop()
+#         tmp_board = deepcopy(board[0])  # Finding this error has taken us ages..
 #         find = find_empty(tmp_board)
 #         print(find)
 
+#         if backtrack:
 #         if not find:
 #             return True
 #         else:
@@ -144,30 +145,6 @@ def solve(board):
 #             solved.pop()
 #             backtrack = True
 
-# def solve(bo):
-#     find = find_empty(bo)
-#     if not find:
-#         return True
-#     else:
-#         row, col = find
-
-#     for i in range(1,10):
-#         if valid(bo, i, (row, col)):
-#             bo[row][col] = i
-
-#             if solve(bo):
-#                 return True
-
-#             bo[row][col] = 0
-
-#     return False
-
-
-def test():
-    minimum = 6
-    for i in range(minimum, 10):
-        print(i)
-
 
 # input/output formaat (ontwikkelend)
 # filename = sys.argv[1]
@@ -175,15 +152,9 @@ def test():
 # filename = "sudoku_boards/simple_6_grid.txt"
 # filename = "sudoku_boards/empty_4_grid.txt"
 # filename = "sudoku_boards/empty_16_grid.txt"
-# filename = "sudoku_boards/16_grid.txt"
-filename = "sudoku_boards/hard_9_grid.txt"
+filename = "sudoku_boards/16_grid.txt"
+# filename = "sudoku_boards/hard_9_grid.txt"
 board = create_sudoku(filename)
 initialize_board(board)
 print_board(board)
-
-# Het algoritme (competend)
-if(solve(board)):
-    print("solution")
-print_board(board)
-
-# test()
+solve(board)
