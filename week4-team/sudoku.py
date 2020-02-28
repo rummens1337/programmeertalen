e81508401a6b9b4817377e87941706007bc16595
Michel <michelrummens@hotmail.com>
2020-02-28 18:55:57 +0900

    tmp

--- week4-team/sudoku.py
+++ week4-team/sudoku.py
@@ -1,6 +1,7 @@
 import sys
 from collections import deque
 from copy import deepcopy
+from itertools import chain
 
 # Declare global variables.
 grid_y_len = 0
@@ -20,7 +21,7 @@ def create_sudoku(filename):
 # Initialize global variables, needed in multiple functions.
 def initialize_board(board):
     global grid_x_len, grid_y_len, sudoku_max_size, sudoku_min_size, sudoku_set
-    
+
     sudoku = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     hexadoku = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
@@ -43,6 +44,8 @@ def initialize_board(board):
     grid_x_len, grid_y_len, sudoku_set, sudoku_min_size = blockSize[sudoku_max_size]
 
 # Prints the board
+
+
 def print_board(board):
     for i in board:
         for n in i:
@@ -60,6 +63,8 @@ def find_empty(board):
     return None
 
 # Checks if num on pos is a valid position
+
+
 def valid(board, num, pos):
     # Check row
     for i in range(sudoku_max_size):
@@ -82,10 +87,51 @@ def valid(board, num, pos):
 
     return True
 
+
+def row_ok(row):
+    for i in row:
+        for j in i:
+            print(i)
+            if sudoku_max_size != len(set(j)):
+                exit("wrong sudokoo")
+
+def grid_ok(line):
+    grid = []
+    grid = ([j for i in line if i for j in i])
+    if len(grid) != 0:
+        print(max(grid)+1 == 10 == len(set([0]+grid)))
+
+
+def check_invalid(board):
+    checks = []
+
+    rows = board
+    columns = [list(a) for a in zip(*board)]
+    grids = []
+
+    for i in range(0, sudoku_max_size + 1, grid_y_len):
+        for j in range(0, sudoku_max_size + 1, grid_x_len):
+            grid = list(chain(row[j:j+grid_x_len]
+                              for row in board[i:i+grid_x_len]))
+            grids.append(grid)
+    [square for square in grids if not grid_ok(square)]
+
+    checks.append(rows)
+    checks.append(columns)
+
+
+
 # Solve the sudoku
+
+
 def solve(board):
     solved = deque()
     solved.append(board)
+
+    if not find_empty(board):
+        check_invalid(board)
+        print_board(board)
+        return True
 
     while solved:
         board = solved.pop()
@@ -148,13 +194,16 @@ def solve(board):
 
 # input/output formaat (ontwikkelend)
 # filename = sys.argv[1]
+
 # filename = "sudoku_boards/21_open_spots_9_grid.txt"
 # filename = "sudoku_boards/simple_6_grid.txt"
 # filename = "sudoku_boards/empty_4_grid.txt"
 # filename = "sudoku_boards/empty_16_grid.txt"
 filename = "sudoku_boards/16_grid.txt"
+# filename = "sudoku_boards/complete_9_grid.txt"
+# filename = "sudoku_boards/wrong_9_grid.txt"
 # filename = "sudoku_boards/hard_9_grid.txt"
 board = create_sudoku(filename)
 initialize_board(board)
-print_board(board)
-solve(board)
+# solve(board)
+solvable(board)
